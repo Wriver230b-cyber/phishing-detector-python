3de7919ad205fe1e1a784de622f1c60b032b987b
Wriver230b-cyber <kennedywilson42@gmail.com>
2026-04-13 20:31:48 -0700

    Delete Phishing_Detector directory

--- Phishing_Detector/config.py
+++
@@ -1,9 +0,0 @@
-# Security Thresholds & Indicators
-SUSPICIOUS_KEYWORDS = [
-    'urgent', 'suspended', 'verify', 'login', 
-    'action required', 'security alert'
-]
-
-# Scoring weights
-RISK_THRESHOLD = 2
-LINK_WEIGHT = 2
